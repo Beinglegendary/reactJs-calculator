0415027d55cbb5fecf2eb8317d4da0dff394765f
Beinglegendary <legendarycolins@yahoo.com>
2023-03-20 13:14:40 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "./App.css";
+
 function App() {
   const [result, setResult] = useState("");
 
@@ -21,66 +22,69 @@ function App() {
   //calculator
   const calc = () => {
     try {
-      setResult(Number(eval(result).toString()).toFixed(2));
+      setResult(
+        Number(new Function("return " + result)().toString()).toFixed(2)
+      );
     } catch (error) {
       setResult("inavalid format");
     }
   };
+
   return (
     <>
-      <div class="container">
-        <div class="calculator">
-          <input type="text" class="calc-numbers" value={result} />
-          <div class="calculator-buttons">
-            <button onClick={clear} class="orange clear span-2">
+      <div className="container">
+        <div className="calculator">
+          <input type="text" className="calc-numbers" value={result} readOnly />
+          <div className="calculator-buttons">
+            <button onClick={clear} className="orange clear span-2">
               AC
             </button>
-            <button onClick={backspace} class="orange">
+            <button onClick={backspace} className="orange">
               &larr;
             </button>
-            <button onClick={handleClick} name="/" class="orange">
+            <button onClick={handleClick} name="/" className="orange">
               &divide;
             </button>
-            <button onClick={handleClick} name="7" class="btn">
+            <button onClick={handleClick} name="7" className="btn">
               7
             </button>
-            <button onClick={handleClick} name="8" class="btn">
+            <button onClick={handleClick} name="8" className="btn">
               8
             </button>
-            <button onClick={handleClick} name="9" class="btn">
+            <button onClick={handleClick} name="9" className="btn">
               9
             </button>
-            <button onClick={handleClick} name="*" class="orange">
+            <button onClick={handleClick} name="*" className="orange">
               x
             </button>
-            <button onClick={handleClick} name="4" class="btn">
+            <button onClick={handleClick} name="4" className="btn">
               4
             </button>
-            <button onClick={handleClick} name="5" class="btn">
+            <button onClick={handleClick} name="5" className="btn">
               5
             </button>
-            <button onClick={handleClick} name="6" class="btn">
+            <button onClick={handleClick} name="6" className="btn">
               6
             </button>
-            <button onClick={handleClick} name="-" class="orange">
+            <button onClick={handleClick} name="-" className="orange">
               -
             </button>
-            <button onClick={handleClick} name="1" class="btn">
+            <button onClick={handleClick} name="1" className="btn">
               1
             </button>
-            <button onClick={handleClick} name="2" class="btn">
+            <button onClick={handleClick} name="2" className="btn">
               2
             </button>
-            <button onClick={handleClick} name="4" class="btn">
+            <button onClick={handleClick} name="3" className="btn">
               3
             </button>
-            <button onClick={handleClick} name="+" class="orange">
+            <button onClick={handleClick} name="+" className="orange">
               +
             </button>
-            <button onClick={handleClick} name="0" class="btn span-3">
+            <button onClick={handleClick} name="0" className="btn span-3">
               0
             </button>
-            <button onClick={calc} class="orange equal">
+            <button onClick={calc} className="orange equal">
               =
             </button>
           </div>
